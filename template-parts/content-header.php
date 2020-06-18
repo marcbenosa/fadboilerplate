7cb11f222cc357c1467d85800285fad5d6dad5ab
Sean Clendening <sean@firstascentdesign.com>
2020-06-18 13:38:29 -0400

    Update Header and Navigation to Bootstrap 4

--- template-parts/content-header.php
+++ template-parts/content-header.php
@@ -1,43 +1,43 @@
 <?php
 /**
- * @package milesscientific
+ * @package fadboilerplate
  */
 
-$company_logo   = get_field('company_logo', 'options');
-$header_logo    = $company_logo['url'];
+$header_logo  = get_field('company_logo', 'options')['url'];
+$footer_logo  = get_field('company_logo_footer', 'options')['url'];
+
+//method="get" action="<?php echo home_url();  " name="s"
 
 ?>
 
 <header id="masthead" class="site-header top<?php echo !is_front_page() ? ' inner-header' : ''; ?>" role="banner">
-    <div class="container-fluid">
-        <nav id="site-navigation" class="navbar navbar-expand-md main-navigation top" role="navigation">
-            <a class="navbar-brand" href="<?php echo home_url(); ?>">
-                <?php
-                    if(substr($header_logo, -4) === ".svg" ) { ?>
-                        <div class="svg-container"><?php echo file_get_contents($header_logo); ?></div>
-                    <?php } else { ?>
-                        <img src="<?php echo $company_logo['url']; ?>" alt="<?php echo $company_logo['name']; ?>" class="nav-logo" />
-                <?php } ?>
-            </a>
-            <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarTogglerDemo" aria-controls="navbarTogglerDemo" aria-expanded="false" aria-label="Toggle navigation">
-                <span class="navbar-toggler-icon"></span>
-            </button>
-            <?php wp_nav_menu(
-                array(
-                'theme_location'=> 'primary',
-                'menu_id' => 'primary-menu',
-                'depth' => 2,
-                'container' => 'div',
-                'container_class' => 'collapse navbar-collapse',
-                'container_id' => 'navbarTogglerDemo',
-                'menu_class' => 'navbar-nav ml-auto',
-                'fallback_cb' => 'WP_Bootstrap_Navwalker::fallback',
-                'walker' => new WP_Bootstrap_Navwalker()
-            ) );
-            ?>
-        </nav>
-        <!-- #site-navigation -->
-    </div>
+    <nav id="site-navigation" class="main-navigation navbar navbar-expand-lg top" role="navigation">
+        <a class="navbar-brand" href="<?php echo home_url(); ?>">
+            <?php
+                if(substr($header_logo, -4) === ".svg" ) { ?>
+                    <div class="svg-container"><?php echo file_get_contents($header_logo); ?></div>
+                <?php } else { ?>
+                    <img src="<?php echo $header_logo; ?>" alt="<?php echo $header_logo; ?>" class="nav-logo" />
+            <?php } ?>
+        </a>
+        <button class="navbar-toggler collapsed d-flex d-lg-none" type="button" data-toggle="collapse" data-target="#main-menu-collapse" aria-controls="main-menu-collapse" aria-expanded="false" aria-label="Toggle navigation">
+            <span class="navbar-toggler-icon__wrapper"><span class="navbar-toggler-icon"></span></span>
+        </button>
+        <?php wp_nav_menu( array(
+            'theme_location'  => 'primary',
+            'menu_id'         => 'primary-menu',
+            'depth'           => 2, // 1 = no dropdowns, 2 = with dropdowns.
+            'container'       => 'div',
+            'container_class' => 'collapse navbar-collapse',
+            'container_id'    => 'main-menu-collapse',
+            'menu_class'      => 'navbar-nav ml-auto',
+            'fallback_cb'     => 'WP_Bootstrap_Navwalker::fallback',
+            'walker'          => new WP_Bootstrap_Navwalker(),
+        ) );
+        ?>
+    </nav>
+    <!-- #site-navigation -->
 </header>
 
 <div id="content" class="site-content">
+    <div class="site-header__spacer top"></div>
