337e0b711db5615b70071fb5d4ed8b55a3c7b9d4
Sean Clendening <sean@firstascentdesign.com>
2020-06-22 08:33:51 -0400

    Add project movefile and remove dcad2020 reference

--- template-parts/content-block-library.php
+++ template-parts/content-block-library.php
@@ -4,7 +4,7 @@
  *
  * @link https://developer.wordpress.org/themes/basics/template-hierarchy/
  *
- * @package dcad2020
+ * @package tevebaugh2020
  */
 
 ?>
@@ -20,7 +20,7 @@
 
 		wp_link_pages(
 			array(
-				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'dcad2020' ),
+				'before' => '<div class="page-links">' . esc_html__( 'Pages:', 'tevebaugh2020' ),
 				'after'  => '</div>',
 			)
 		);
@@ -34,7 +34,7 @@
 				sprintf(
 					wp_kses(
 						/* translators: %s: Name of current post. Only visible to screen readers */
-						__( 'Edit <span class="screen-reader-text">%s</span>', 'dcad2020' ),
+						__( 'Edit <span class="screen-reader-text">%s</span>', 'tevebaugh2020' ),
 						array(
 							'span' => array(
 								'class' => array(),
