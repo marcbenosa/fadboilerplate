3c8f6b4c9e28ff844affa66896f1cc8936b57496
John Himics <john@firstascentdesign.com>
2020-07-15 23:17:30 -0400

    added gutenberg editor colors to stylesheet and php

--- blocks/acf-blocks.php
+++ blocks/acf-blocks.php
@@ -15,45 +15,28 @@ function my_block_category( $categories, $post ) {
 }
 add_filter( 'block_categories', 'my_block_category', 10, 2);
 
+function fadboilerplate_gutenberg_theme_supports() {
+	//https://weblines.com.au/gutenberg-blocks-wide-alignment-full-width/
+	add_theme_support( 'align-wide' );
+
+	// Disable Custom Colors
+	// add_theme_support( 'disable-custom-colors' );
+
+	// Editor Color Palette
+	// add_theme_support( 'editor-color-palette', array(
+	// 	array(
+	// 		'name'  => __( 'Dark Blue', 'ea-starter' ),
+	// 		'slug'  => 'darkblue',
+	// 		'color'	=> '#0c131e',
+	// 	),
+	// ) );
+}
+add_action( 'after_setup_theme', 'fadboilerplate_gutenberg_theme_supports' );
+
 
 //reference: https://www.advancedcustomfields.com/resources/acf_register_block_type/
 function register_acf_block_types() {
 
-	/*
-	// register a testimonial block.
-	name
-	(String) A unique name that identifies the block (without namespace). For example ‘testimonial’. Note: A block name can only contain lowercase alphanumeric characters and dashes, and must begin with a letter.
-	title
-	(String) The display title for your block. For example ‘Testimonial’.
-	description
-	(String) (Optional) This is a short description for your block.
-	category
-	(String) Blocks are grouped into categories to help users browse and discover them. The core provided categories are [ common | formatting | layout | widgets | embed ]. Plugins and Themes can also register custom block categories.
-	icon
-	(String|Array) (Optional) An icon property can be specified to make it easier to identify a block. These can be any of WordPress’ Dashicons, or a custom svg element.
-	keywords
-	(Array) (Optional) An array of search terms to help user discover the block while searching.
-	post_types
-	(Array) (Optional) An array of post types to restrict this block type to.
-	mode
-	(String) (Optional) The display mode for your block. Available settings are “auto”, “preview” and “edit”. Defaults to “preview”. auto: Preview is shown by default but changes to edit form when block is selected. preview: Preview is always shown. Edit form appears in sidebar when block is selected. edit: Edit form is always shown.
-	Note. When in “preview” or “edit” modes, an icon will appear in the block toolbar to toggle between modes.
-	align
-	(String) (Optional) The default block alignment. Available settings are “left”, “center”, “right”, “wide” and “full”. Defaults to an empty string.
-	render_template
-	(String) The path to a template file used to render the block HTML. This can either be a relative path to a file within the active theme or a full path to any file.
-	render_callback
-	(Callable) (Optional) Instead of providing a render_template, a callback function name may be specified to output the block’s HTML.
-	enqueue_style
-	(String) (Optional) The url to a .css file to be enqueued whenever your block is displayed (front-end and back-end).
-	enqueue_script
-	(String) (Optional) The url to a .js file to be enqueued whenever your block is displayed (front-end and back-end).
-	enqueue_assets
-	(Callable) (Optional) A callback function that runs whenever your block is displayed (front-end and back-end) and enqueues scripts and/or styles.
-	supports
-	(Array) (Optional) An array of features to support. All properties from the JavaScript block supports documentation may be used. The following options are supported:
-
-	*/
 	acf_register_block_type(array(
 	    'name'              => 'example',
 	    'title'             => __('Example'),
