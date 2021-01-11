7cc23190f19cf801ca1696564cb1194dc0fd5baf
Sean Clendening <sean@firstascentdesign.com>
2021-01-10 21:22:14 -0500

    Simplify footer and update ACF

--- template-parts/content-footer.php
+++ template-parts/content-footer.php
@@ -8,9 +8,6 @@
  *
  * @package wilminvest
  */
-
-$footer_logo  = get_field('company_logo_footer', 'options')['url'];
-$address      = get_field('address', 'options');
 $contact      = get_field('contact', 'options');
 $social       = get_field('social_media', 'options');
 $links        = get_field('footer_links', 'options');
@@ -19,40 +16,8 @@ $newsletter   = get_field('newsletter_shortcode', 'options');
 ?>
 <footer id="colophon" class="site-footer" role="contentinfo">
     <div class="site-info container">
-        <div class="pre-footer row">
-
-        </div>
         <div class="footer row">
-            <div class="col-sm-3 mr-auto">
-                <a class="footer__brand" href="<?php echo home_url(); ?>">
-                    <?php
-                        if(substr($footer_logo, -4) === ".svg" ) { ?>
-                            <div class="svg-container"><?php echo file_get_contents($footer_logo); ?></div>
-                        <?php } else { ?>
-                            <img src="<?php echo $footer_logo; ?>" alt="<?php echo $footer_logo; ?>" class="nav-logo" />
-                    <?php } ?>
-                </a>
-            </div>
             <div class="col-sm-3">
-            	<div class="footer__address">
-                    <div><?php echo esc_html( $address['location1'] );?></div>
-                    <div><?php echo esc_html( $address['location2'] );?></div>
-                </div>
-
-                <?php if( have_rows('operational_hours', 'options') ): ?>
-                    <div class="footer__operational">
-                    <?php while ( have_rows('operational_hours', 'options') ) : the_row();
-                        $days  = get_sub_field('days');
-                        $hours = get_sub_field('hours');
-                        if($days !== ""): ?>
-	                        <div class="d-block">
-	                            <span><?php echo esc_html( $days ); ?></span>: <span><?php echo esc_html( $hours ); ?></span>
-	                        </div>
-                    	<?php endif; ?>
-                    <?php endwhile; ?>
-                    </div>
-                <?php endif; ?>
-
                 <div class="footer__contact">
                     <?php if($contact['email'] !== ""): ?>
                     	<a class="d-block" href="mailto:<?php echo esc_attr( $contact['email'] ); ?>"><?php echo esc_html( $contact['email'] );?></a>
@@ -78,7 +43,7 @@ $newsletter   = get_field('newsletter_shortcode', 'options');
                 ) );
                 ?>
             </div>
-            <div class="col-sm-3">
+            <div class="col-sm-6">
                 <?php echo do_shortcode( $newsletter ); ?>
             </div>
         </div>
